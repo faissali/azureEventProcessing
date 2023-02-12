5635bacbd8821a0218459a983ceb25be021ac5c6
Fayçal BZIOU <faycal.bziou@exakis-nelite.com>
2023-02-12 17:43:16 +0100

    add eventhub trigger

--- EventProcessingFunction.cs
+++ EventProcessingFunction.cs
@@ -7,29 +7,21 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Text;
+using Azure.Messaging.EventHubs;
 
 namespace EventProcessingFunction
 {
     public static class EventProcessingFunction
     {
         [FunctionName("EventProcessingFunction")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+        public static void Run([EventHubTrigger("telemetries", Connection = "EventHubConnectionAppSetting", ConsumerGroup = "$Default")] EventData[] eventHubMessages, ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            foreach (var message in eventHubMessages)
+            {
+                log.LogInformation($"C# function triggered to process a message: {Encoding.UTF8.GetString(message.EventBody)}");
+                log.LogInformation($"EnqueuedTimeUtc={message.EnqueuedTime}");
+            }
         }
     }
 }
