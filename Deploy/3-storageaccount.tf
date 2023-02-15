62b3493f923f066fd78bbc2299a67c2dd3e78853
Fayçal BZIOU <faycal.bziou@exakis-nelite.com>
2023-02-15 21:59:35 +0100

    add terraform

---
+++ Deploy/3-storageaccount.tf
@@ -0,0 +1,26 @@
+
+# lectures utiles : https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_account
+#                   https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/storage_container
+
+
+resource "azurerm_storage_account" "Terra-Storage-Backend" {
+  name                      = var.storageAccountBackendTerraform
+  resource_group_name       = azurerm_resource_group.Terra_tfbackend_rg.name
+  location                  = azurerm_resource_group.Terra_tfbackend_rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  account_kind              = "StorageV2"
+  enable_https_traffic_only = true
+
+  tags = {
+    environment = "lab",
+    owner = "faycal.bziou@exakis-nelite.com"
+  }
+}
+
+
+resource "azurerm_storage_container" "Terra-Container-Storage" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.Terra-Storage-Backend.name
+  container_access_type = "private"
+}
---
+++ Deploy/4-variables.tf
@@ -0,0 +1,24 @@
+variable "azureRegion" {
+  description = "Azure Region where to deploy resources. Caution the region must support Availability Zone"
+  # To get names of Azure Region : az account list-locations
+  # To check support of Availability Zone in the Azure Region see https://docs.microsoft.com/bs-latn-ba/azure/availability-zones/az-overview
+  type    = string
+  default = "westeurope"
+}
+
+#  Resource Group Name
+variable "resourceGroupName" {
+  type    = string
+  default = "RG-AKSClusterfbu"
+}
+
+variable "ownerTag" {
+  type    = string
+  default = "faycal.bziou@exakis-nelite.com"
+}
+
+#  Storage Account Name
+variable "storageAccountBackendTerraform" {
+  type    = string
+  default = "terraformstanfbu" # IMPORTANT mettre ici un nom unique. cf https://docs.microsoft.com/en-us/azure/cloud-adoption-framework/ready/azure-best-practices/resource-naming#example-names-storage
+}
