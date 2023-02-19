0467472f714fe18b7d6e07bb968855ea50296e31
Fayçal BZIOU <faycal.bziou@exakis-nelite.com>
2023-02-19 12:44:54 +0100

    update terraform

--- Deploy/1-versions.tf
+++ Deploy/1-versions.tf
@@ -1,12 +1,3 @@
-#   _______                   __                                           _                                              _     _               
-#  |__   __|                 / _|                                         (_)               ___                          (_)   | |              
-#     | | ___ _ __ _ __ __ _| |_ ___  _ __ _ __ ___    __   _____ _ __ ___ _  ___  _ __    ( _ )     _ __  _ __ _____   ___  __| | ___ _ __ ___ 
-#     | |/ _ \ '__| '__/ _` |  _/ _ \| '__| '_ ` _ \   \ \ / / _ \ '__/ __| |/ _ \| '_ \   / _ \/\  | '_ \| '__/ _ \ \ / / |/ _` |/ _ \ '__/ __|
-#     | |  __/ |  | | | (_| | || (_) | |  | | | | | |   \ V /  __/ |  \__ \ | (_) | | | | | (_>  <  | |_) | | | (_) \ V /| | (_| |  __/ |  \__ \
-#     |_|\___|_|  |_|  \__,_|_| \___/|_|  |_| |_| |_|    \_/ \___|_|  |___/_|\___/|_| |_|  \___/\/  | .__/|_|  \___/ \_/ |_|\__,_|\___|_|  |___/
-#                                                                                                   | |                                         
-#                                                                                                   |_|                                         
-
 terraform {
   required_version = "= 1.3.5"
   backend "azurerm" {
@@ -14,7 +5,7 @@ terraform {
     storage_account_name = "terraformstoragefbu"      # mettre le nom du compte de stockage créer dans le lab 1
     container_name       = "terraform"
     key                  = "test"
-    access_key           = "xx"#"__tf_storage_account_key__"
+    access_key           = "xx"#"xxtf_storage_account_keyxx"
   }
   required_providers {
     azurerm = {
